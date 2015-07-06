fffbaac29a73b3f1d45ebdf06631a887dec6f699
SXB <1398707587@qq.com>
2015-07-07 02:53:05 +0800

    Initial Commit

---
+++ c++初体验/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++初体验
+//
+//  Created by SXB on 15/7/7.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
